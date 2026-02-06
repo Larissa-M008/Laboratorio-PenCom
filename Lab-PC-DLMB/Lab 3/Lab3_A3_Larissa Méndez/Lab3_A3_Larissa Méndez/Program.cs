c8c18f6e43c85ac94e860d974e45e7fa8a3bf3f7
Larissa-M008 <darlenelarissa.mb2008@gmail.com>
2026-02-05 19:30:14 -0600

    Add files via upload

---
+++ Lab-PC-DLMB/Lab 3/Lab3_A3_Larissa Méndez/Lab3_A3_Larissa Méndez/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_A3_Larissa_Méndez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab 3, Pensamiento Computacional, Larissa Méndez");
+            Console.WriteLine("Ingrese su nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el nombre de su curso");
+            string curso = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre);
+            Console.WriteLine("eres estudiante del curso de " + curso);
+            Console.WriteLine("Presione cualquier tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
+
